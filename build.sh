9c23dabc70cf3e177c9def9a04583266d6cd3fcd
B. van Wetten <bas.van.wetten@gmail.com>
2020-03-28 22:27:13 +0100

    Updated several files

--- build.sh
+++ build.sh
@@ -1,8 +1,27 @@
+#!/usr/bin/env sh
+
+# Get latest version from screeps github repository
 version=$(curl https://raw.githubusercontent.com/screeps/screeps/master/package.json | jq -r .version)
+
+# Clone our repository
 git clone git@github.com:QNimbus/docker-screeps.git
+
+# Push CWD onto stack and cd into repository directory
 pushd docker-screeps
+
+# Update version number in Dockerfile and create/update timestamp
 sed -i 's/^ENV SCREEPS_VERSION.*$/ENV SCREEPS_VERSION '$version'/i' Dockerfile
-git commit -a -m "Auto Update to screeps "$version
+date > timestamp
+git add timestamp
+
+# Commit, tag and push updated repository
+git commit --no-gpg-sign -a -m "Auto Update to screeps "$version
+git tag -f $version
+if [[ "$version" != "ptr" ]]; then
+	git tag -f latest
+fi
 git push origin master --tags
+
+# Go back to original folder and remove repository
 popd
 rm -rf docker-screeps
